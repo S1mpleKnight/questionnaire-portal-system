9d9e8f1939247171ec3f88d6ac92a83bbd0e6de5
S1mpleKnight <become.adult77@gmail.com>
2022-04-13 14:10:06 +0300

    new: init project

---
+++ src/main/java/by/softarex/internship/task/questionnaireportalsystem/QuestionnairePortalSystemApplication.java
@@ -0,0 +1,13 @@
+package by.softarex.internship.task.questionnaireportalsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnairePortalSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionnairePortalSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/softarex/internship/task/questionnaireportalsystem/QuestionnairePortalSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package by.softarex.internship.task.questionnaireportalsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnairePortalSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
