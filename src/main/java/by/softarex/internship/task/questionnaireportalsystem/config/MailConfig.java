8873190c4b901d1ce52f17e2370f19ebffa8e223
S1mpleKnight <become.adult77@gmail.com>
2022-04-20 02:37:39 +0300

    new: create mail/security configs

---
+++ src/main/java/by/softarex/internship/task/questionnaireportalsystem/config/MailConfig.java
@@ -0,0 +1,46 @@
+package by.softarex.internship.task.questionnaireportalsystem.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+    @Value("${spring.mail.host}")
+    private String host;
+    @Value("${spring.mail.port}")
+    private Integer port;
+    @Value("${spring.mail.username}")
+    private String username;
+    @Value("${spring.mail.password}")
+    private String password;
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private Boolean auth;
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private Boolean starttls;
+    @Value("${spring.mail.protocol}")
+    private String protocol;
+    @Value("${mail.debug}")
+    private Boolean debug;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", protocol);
+        props.put("mail.smtp.auth", auth);
+        props.put("mail.smtp.starttls.enable", starttls);
+        props.put("mail.debug", debug);
+
+        return mailSender;
+    }
+}
