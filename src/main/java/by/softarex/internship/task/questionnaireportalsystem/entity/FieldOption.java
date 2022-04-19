b88dd594ce526a54ca67c4f6cede97ee8f3dc6ca
S1mpleKnight <become.adult77@gmail.com>
2022-04-20 02:39:49 +0300

    fix: correct FieldOption class (value constructor)

--- src/main/java/by/softarex/internship/task/questionnaireportalsystem/entity/FieldOption.java
+++ src/main/java/by/softarex/internship/task/questionnaireportalsystem/entity/FieldOption.java
@@ -1,7 +1,9 @@
 package by.softarex.internship.task.questionnaireportalsystem.entity;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -14,12 +16,20 @@ import javax.persistence.Table;
 @Setter
 @Entity
 @Table(name="field_options")
+@ToString
+@NoArgsConstructor
 public class FieldOption {
     @Id
     @GeneratedValue
+    @ToString.Exclude
     private Long id;
     private String value;
+    @ToString.Exclude
     @ManyToOne
     @JoinColumn(name = "field_id")
     private Field field;
+
+    public FieldOption(String value) {
+        this.value = value;
+    }
 }
