cf5d76c26268e04d9a1854527f19a5f923fe36bb
S1mpleKnight <become.adult77@gmail.com>
2022-04-21 19:23:46 +0300

    fix: add questionnaire, field, field option & response repositories methods

--- src/main/java/by/softarex/internship/task/questionnaireportalsystem/repository/QuestionnaireRepository.java
+++ src/main/java/by/softarex/internship/task/questionnaireportalsystem/repository/QuestionnaireRepository.java
@@ -4,8 +4,10 @@ import by.softarex.internship.task.questionnaireportalsystem.entity.Questionnair
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface QuestionnaireRepository extends CrudRepository<Questionnaire, UUID> {
+    Optional<Questionnaire> findByUser_Id(UUID userId);
 }
