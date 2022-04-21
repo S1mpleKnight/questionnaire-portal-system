cdfe5e5897721050684103429cc2fab180ca80de
S1mpleKnight <become.adult77@gmail.com>
2022-04-21 19:24:08 +0300

    new: create ResponseDto

---
+++ src/main/java/by/softarex/internship/task/questionnaireportalsystem/dto/ResponseDto.java
@@ -0,0 +1,11 @@
+package by.softarex.internship.task.questionnaireportalsystem.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseDto {
+    private String value;
+    private String fieldPosition;
+}
