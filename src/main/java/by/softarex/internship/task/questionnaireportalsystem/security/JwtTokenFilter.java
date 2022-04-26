e571bf797025573fffbc5d6ff814c2461049a7c1
S1mpleKnight <become.adult77@gmail.com>
2022-04-27 01:29:33 +0300

    new: create JWT logic

---
+++ src/main/java/by/softarex/internship/task/questionnaireportalsystem/security/JwtTokenFilter.java
@@ -0,0 +1,52 @@
+package by.softarex.internship.task.questionnaireportalsystem.security;
+
+import by.softarex.internship.task.questionnaireportalsystem.exception.JwtAuthenticationException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Slf4j
+@AllArgsConstructor
+public class JwtTokenFilter extends OncePerRequestFilter {
+    private final JwtTokenProvider tokenProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        String token = tokenProvider.resolveToken(httpServletRequest);
+        try {
+            acceptAuthentication(token);
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+        } catch (JwtAuthenticationException e) {
+            rejectAuthentication(e);
+        }
+    }
+
+    private void rejectAuthentication(JwtAuthenticationException e) {
+        log.error(e.getMessage());
+        SecurityContextHolder.clearContext();
+        throw new JwtAuthenticationException("JWT token filter have not been passed");
+    }
+
+    private void acceptAuthentication(String token) {
+        if (isTokenValid(token)) {
+            Authentication authentication = tokenProvider.getAuthentication(token);
+            if (authentication != null) {
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+    }
+
+    private boolean isTokenValid(String token) {
+        return token != null && tokenProvider.validateToken(token);
+    }
+}
---
+++ src/main/java/by/softarex/internship/task/questionnaireportalsystem/service/JwtAuthenticationService.java
@@ -0,0 +1,37 @@
+package by.softarex.internship.task.questionnaireportalsystem.service;
+
+import by.softarex.internship.task.questionnaireportalsystem.dto.AuthenticationRequestDto;
+import by.softarex.internship.task.questionnaireportalsystem.security.JwtTokenProvider;
+import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@Service
+@AllArgsConstructor
+public class JwtAuthenticationService {
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenProvider tokenProvider;
+    private final UserService userService;
+
+    public Map<Object, Object> authenticate(AuthenticationRequestDto request) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        return fillMap(request, createToken(request));
+    }
+
+    private String createToken(AuthenticationRequestDto request) {
+        UUID id = userService.findIdByEmail(request.getEmail());
+        return tokenProvider.createToken(request.getEmail(), id.toString());
+    }
+
+    private Map<Object, Object> fillMap(AuthenticationRequestDto request, String token) {
+        Map<Object, Object> response = new HashMap<>();
+        response.put("id", request.getEmail());
+        response.put("token", token);
+        return response;
+    }
+}
