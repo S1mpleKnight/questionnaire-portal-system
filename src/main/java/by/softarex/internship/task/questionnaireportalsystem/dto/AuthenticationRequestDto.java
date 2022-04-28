0ea7a94af904a032b946b27c0eda836b9140cb8f
S1mpleKnight <become.adult77@gmail.com>
2022-04-28 03:07:46 +0300

    fix: add validation to the rest dto

--- src/main/java/by/softarex/internship/task/questionnaireportalsystem/dto/AuthenticationRequestDto.java
+++ src/main/java/by/softarex/internship/task/questionnaireportalsystem/dto/AuthenticationRequestDto.java
@@ -5,11 +5,18 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @ToString
 @AllArgsConstructor
 public class AuthenticationRequestDto {
+    @Email
     private String email;
+    @Size(max = 255, min = 10, message = "Enter a valid password")
+    @NotBlank(message = "Enter a valid password")
     private String password;
 }
--- src/main/java/by/softarex/internship/task/questionnaireportalsystem/dto/ChangePasswordDto.java
+++ src/main/java/by/softarex/internship/task/questionnaireportalsystem/dto/ChangePasswordDto.java
@@ -4,10 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Setter
 @Getter
 @ToString
 public class ChangePasswordDto {
+    @Size(max = 255, min = 10, message = "Enter a valid password")
+    @NotBlank(message = "Old password is empty")
     private String oldPassword;
+    @Size(max = 255, min = 10, message = "Enter a valid password")
+    @NotBlank(message = "New password is empty")
     private String newPassword;
 }
